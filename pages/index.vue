835752c90570a3ff2c6bf2e8e2b2162a2feb3d90
Mathias Moser <mathias.moser@betsnaps.com>
2021-01-23 13:51:06 +0100

    auth

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,13 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
 
 <style>
